8217511cb1584cf887c08417bbc951848d8321ce
shaheen-728 <khanshaheen728@gmail.com>
2026-05-01 19:41:25 +0530

    Typo correct

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+print("DB URL:", os.getenv("DATABASE_URL"))
 engine = create_engine(DATABASE_URL)
 session = sessionmaker(bind=engine,autoflush=False,autocommit=False)
 
