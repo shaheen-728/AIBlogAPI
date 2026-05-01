6b4e0bdabc630751b37ba5a2c4aa26842d85441a
shaheen-728 <khanshaheen728@gmail.com>
2026-05-01 16:12:12 +0530

    add secret key in os

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
 from fastapi.concurrency import asynccontextmanager
 
-from database import Base, engine
 from fastapi import  FastAPI
 
 from routers import posts, users
--- utils/auth.py
+++ utils/auth.py
@@ -1,43 +1,46 @@
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
+import os
 
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-SECRET_KEY = "your-secret-key"   # change later
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY not set")
+
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    if "sub" in to_encode:
+        to_encode["sub"] = str(to_encode["sub"])
 
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-    return encoded_jwt
 
 def verify_token(token: str):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
     except JWTError:
-        return None
-    
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+
 def get_current_user(token: str = Depends(oauth2_scheme)):
     payload = verify_token(token)
 
-    if payload is None:
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid token"
-        )
-
     user_id = payload.get("sub")
 
-    return user_id
+    if user_id is None:
+        raise HTTPException(status_code=401, detail="Invalid token payload")
+
+    return int(user_id)
